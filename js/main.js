a5492d2d841ea928e061f8d04bd8bd7e9d9a9624
Shelley Arnold <shelle.kay@gmail.com>
2019-04-11 09:24:32 -0500

    added loop options

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ welcome.textContent = "Welcome to the Yaddle Blog";
 const allArticles = document.querySelectorAll(".article__header");
 allArticles[0].classList.add("important");
 allArticles[1].classList.add("important");
+// Use a For Loop
+for(let i = 0; i < allArticles.length; i++) {
+    allArticles[i].classList.add("forLoop");
+}
+// Use a ForEach Loop
+allArticles.forEach (taco => {
+    taco.classList.add("forEach");
+})
 
 const noDash = document.querySelector(".dashed");
 noDash.classList.remove("dashed");
